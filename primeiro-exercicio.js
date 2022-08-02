fdd9843a51e4c4f2311b94241ebcb331d478b03d
ongacarol <onga.carol@gmail.com>
2022-08-02 00:39:03 -0300

    updates

--- primeiro-exercicio.js
+++ primeiro-exercicio.js
@@ -1,3 +1,7 @@
+// 1 Extraia o nome da tabela e armazene em uma variável chamada "tableName".
+// 2 Extraia as colunas da tabela e armazene em uma variável chamada "columns".
+// 3 Manipule a variável "columns", separando cada coluna com seu respectivo tipo, em uma string separada.
+
 const statement="create table author (id number, name string, age number, city string, state string, country string)";
 const regexp = /create table ([a-z]+) \((.+)\)/;
 const parsedStatement =statement.match(regexp);
